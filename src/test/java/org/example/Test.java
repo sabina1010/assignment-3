1ead84edd94900b695c03c832b867be1cd1f364a
sabina1010 <109016620+sabina1010@users.noreply.github.com>
2023-11-20 19:01:35 +0400

    salam

--- src/test/java/org/example/Test.java
+++ src/test/java/org/example/Test.java
@@ -82,7 +82,7 @@ public class Test {
         Assert.assertEquals("Please enter a username and password.", error.getText());
     }
     @org.junit.Test
-    public void wrongCredentials() throws Exception{
+    public void false_credentials() throws Exception{
         Thread.sleep(1000);
         WebElement username=driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[1]/input"));
         WebElement password=driver.findElement(By.xpath("//*[@id=\"loginPanel\"]/form/div[2]/input"));
@@ -100,7 +100,7 @@ public class Test {
         Assert.assertEquals("READ MORE", error.getText());
     }
     @org.junit.Test
-    public void aboutUs() throws Exception{
+    public void about_us_part() throws Exception{
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[2]/a")).click();
         WebElement title=driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/h1"));
@@ -113,13 +113,7 @@ public class Test {
         WebElement title=driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/span[1]"));
         Assert.assertEquals("Available Bookstore SOAP services:", title.getText());
     }
-@org.junit.Test
-public void admin()throws Exception{
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[6]/a")).click();
-    WebElement title=driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/h1"));
-    Assert.assertFalse(("Administration".equals( title.getText())));
-}
+
     @org.junit.Test
     public void index() throws Exception{
             Thread.sleep(1000);
@@ -132,14 +126,9 @@ public void admin()throws Exception{
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[4]/a")).click();
         Assert.assertEquals("https://www.parasoft.com/products/", driver.getCurrentUrl());
     }
+
     @org.junit.Test
-    public void locations() throws Exception{
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/ul[1]/li[5]/a")).click();
-        Assert.assertEquals("https://www.parasoft.com/solutions/", driver.getCurrentUrl());
-    }
-    @org.junit.Test
-    public void transferLoan() throws Exception{
+    public void transfer_loan() throws Exception{
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/ul[1]/li[3]/a")).click();
         Assert.assertSame("https://parabank.parasoft.com/parabank/services/ParaBank?wsdl", driver.getCurrentUrl());
